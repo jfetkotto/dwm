efee1fb501596c39fc0cc0ef44c3a14dd3dede91
Sam Mitchell <jfetkotto@gmail.com>
2022-12-16 20:06:24 +0000

    Greyscale

--- config.h
+++ config.h
@@ -5,18 +5,18 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#101010";
+static const char col_gray2[]       = "#454545";
+static const char col_gray3[]       = "#999999";
+static const char col_gray4[]       = "#b9b9b9";
+static const char col_gray5[]        = "#252525";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+        /*               fg         bg         border   */
+        [SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+        [SchemeSel]  = { col_gray4, col_gray5,  col_gray2  },
 };
 
+static const char dmenufont[]       = "Iosevka:size=10:style=bold:antialias=true";
 static const char *fonts[]          = {
 	"Iosevka:antialias=true:size=10:style=bold",
 	"Font Awesome 5 Free:antialias=true:size=9:style=solid",
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
